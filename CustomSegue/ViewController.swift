a55a593fcbf1b4661e3731d23a74d1e006b25e9e
mmilord <info@parleychat.com>
2018-06-28 18:54:06 +0200

    Initial Commit

---
+++ CustomSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSegue
+//
+//  Created by Mike Milord on 28/06/2018.
+//  Copyright © 2018 First Republic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
